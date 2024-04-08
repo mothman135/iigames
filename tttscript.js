d51065f5832e9b07a7c5da372d36d6d5ed05e9af
mothman135 <iwo.wo.edu@gmail.com>
2024-04-08 19:09:17 +0200

    basic tic tac toe

--- tttscript.js
+++ tttscript.js
@@ -7,8 +7,8 @@ function handlePlayerTurn(clickedCellIndex) {
       return;
   }
   gameBoard[clickedCellIndex] = currentPlayer;
-  if(currentPlayer == "X") currentPlayer = "O";
-  else currentPlayer = "X";
+  checkForWinOrDraw();
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 }
 
 function cellClicked(clickedCellEvent) {
@@ -32,3 +32,62 @@ function updateUI() {
       cells[i].innerText = gameBoard[i];
   }
 }
+
+function announceWinner(player) {
+  const messageElement = document.getElementById('gameMessage');
+  messageElement.innerText = `Player ${player} Wins!`;
+}
+
+function announceDraw() {
+  const messageElement = document.getElementById('gameMessage');
+  messageElement.innerText = 'Game Draw!';
+}
+
+const winConditions = [
+  [0, 1, 2], // Top row
+  [3, 4, 5], // Middle row
+  [6, 7, 8], // Bottom row
+  [0, 3, 6], // Left column
+  [1, 4, 7], // Middle column
+  [2, 5, 8], // Right column
+  [0, 4, 8], // Left-to-right diagonal
+  [2, 4, 6]  // Right-to-left diagonal
+];
+
+function checkForWinOrDraw() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+      const [a, b, c] = winConditions[i];
+      if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+          roundWon = true;
+          break;
+      }
+  }
+
+  if (roundWon) {
+      announceWinner(currentPlayer);
+      gameActive = false;
+      return;
+  }
+
+  let roundDraw = !gameBoard.includes('');
+  if (roundDraw) {
+      announceDraw();
+      gameActive = false;
+      return;
+  }
+}
+
+function resetGame() {
+  gameBoard = ['', '', '', '', '', '', '', '', ''];
+  gameActive = true;
+  currentPlayer = 'X';
+  cells.forEach(cell => {
+      cell.innerText = '';
+  });
+  document.getElementById('gameMessage').innerText = '';
+}
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetGame, false);
